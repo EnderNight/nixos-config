a7140ce705418382b6cc73bb226fdbcd3e426a96
Ender <ndxendernight@gmail.com>
2025-04-27 04:13:03 +0200

    feat: add kitty theme and fonts

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -1,5 +1,16 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  programs.kitty.enable = true;
+  programs.kitty = {
+    enable = true;
+    themeFile = "OneDark-Pro";
+    font = {
+      name = "FiraCode Nerd Font";
+      package = (pkgs.nerdfonts.override {
+        fonts = [
+	  "FiraCode"
+	];
+      });
+    };
+  };
 }
