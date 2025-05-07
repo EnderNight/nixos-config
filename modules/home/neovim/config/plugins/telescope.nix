2d1c5a62437a1d1345bdad35c150c9bf85c91509
Ender <ndxendernight@gmail.com>
2025-05-07 02:35:29 +0200

    refactor: reformat

--- modules/home/neovim/config/plugins/telescope.nix
+++ modules/home/neovim/config/plugins/telescope.nix
@@ -7,20 +7,20 @@
     keymaps = {
       "<leader>ff" = {
         action = "find_files";
-	options = {
-	  desc = "Telescope find files";
-	  noremap = true;
-	  silent = true;
-	};
+        options = {
+          desc = "Telescope find files";
+          noremap = true;
+          silent = true;
+        };
       };
 
       "<leader>fg" = {
         action = "live_grep";
-	options = {
-	  desc = "Telescope live grep";
-	  noremap = true;
-	  silent = true;
-	};
+        options = {
+          desc = "Telescope live grep";
+          noremap = true;
+          silent = true;
+        };
       };
     };
   };
